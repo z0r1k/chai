737c2a5ff0442125cca6fa3537708b5261296e9e
Apprentice <apprentice@devbootcamp.com>
2012-11-26 15:05:57 -0800

    first commit of empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chai::Application.config.secret_token = '9d191e295ff1dd90963d9b484a7c7614b0ee43858138670798ded2e3fe3f09da3246b892765dd225406683126de29e1046c144253b3f45f1d954f189611a4d6c'
