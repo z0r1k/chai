1e505c57fa90f6d8b1b2623ded20c76929d465d1
Apprentice <apprentice@devbootcamp.com>
2012-11-26 15:32:00 -0800

    generated resources for User, Visit, Shop

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Chai::Application.routes.draw do
+  resources :visits
+
+  resources :shops
+
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
