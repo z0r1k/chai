5a340bb4a682911025c2bb51772ae4fade7d9dcf
Ferdi Cam <flakyfilibuster@gmail.com>
2012-11-28 17:34:26 -0800

    refactored shop controller

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -19,30 +19,6 @@ class ShopsController < ApplicationController
 
     @results[:businesses].each  do |shop|
       Shop.update_or_create_by_name_and_latitude_and_longitude shop.except(:distance, :review_count)
-      # @shop = Shop.find_by_name_and_latitude_and_longitude(shop[:name],shop[:latitude],shop[:longitude])
-      # if @shop
-      #   @shop.update_attributes(name: shop[:name],
-      #                           latitude: shop[:latitude],
-      #                           longitude: shop[:longitude],
-      #                           rating: shop[:rating],
-      #                           yelp_url: shop[:url],
-      #                           img_url: shop[:image_url])
-
-      # else
-      #   @shop = Shop.new
-      #   @shop.name = shop[:name]
-      #   @shop.latitude = shop[:latitude]
-      #   @shop.longitude = shop[:longitude]
-      #   @shop.rating = shop[:rating]
-      #   @shop.yelp_url = shop[:url]
-      #   @shop.img_url = shop[:image_url]
-      #   logger.info "------------------"
-      #   logger.info "shop info:"
-      #   logger.info @shop.yelp_url
-      #   logger.info @shop.img_url
-      #   logger.info "------------------"
-      #   @shop.save
-      # end
     end
 
 
